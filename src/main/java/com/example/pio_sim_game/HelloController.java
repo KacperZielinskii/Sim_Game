fc6aa936063b043f880577eb89abbdb3dcc213f9
Bartosz Pawlowski <235949@edu.p.lodz.pl>
2022-05-24 14:28:21 +0200

    Project commit

---
+++ src/main/java/com/example/pio_sim_game/HelloController.java
@@ -0,0 +1,16 @@
+package com.example.pio_sim_game;
+
+//Do usuniecia
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.example.pio_sim_game {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.pio_sim_game to javafx.fxml;
+    opens com.example.pio_sim_game.controllers to javafx.fxml;
+    exports com.example.pio_sim_game;
+}

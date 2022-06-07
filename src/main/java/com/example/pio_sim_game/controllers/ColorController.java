22adc4cc261357825c0b57d3886b500458fc09a1
Bartosz Pawlowski <235949@edu.p.lodz.pl>
2022-06-07 13:59:03 +0200

    Refaktoryzacja kodu i wybor kolorow

--- src/main/java/com/example/pio_sim_game/controllers/ColorController.java
+++ src/main/java/com/example/pio_sim_game/controllers/ColorController.java
@@ -2,11 +2,12 @@ package com.example.pio_sim_game.controllers;
 
 import com.example.pio_sim_game.models.GameModel;
 import javafx.event.ActionEvent;
-import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.ColorPicker;
+import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class ColorController {
@@ -16,14 +17,10 @@ public class ColorController {
 
     @FXML private ColorPicker playerColor1;
     @FXML private ColorPicker playerColor2;
+    @FXML private Label errorMsg;
 
     public void initModel(GameModel model) {
         this.model = model;
-
-        playerColor1.valueProperty().bind(this.model.playerColor1Property());
-        playerColor2.valueProperty().bind(this.model.playerColor2Property());
-
-        //Nazwy graczy
     }
 
     public void setNextScene(Scene scene) {
@@ -32,8 +29,27 @@ public class ColorController {
     @FXML
     private void GoToGame(ActionEvent event)
     {
-        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        stage.setScene(scene);
-        //initGame
+        if(!validateColors(toRGBCode(playerColor1.getValue()), toRGBCode(playerColor2.getValue()))) {
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(scene);
+            this.model.setColors(toRGBCode(playerColor1.getValue()), toRGBCode(playerColor2.getValue()));
+        }
+        else {
+            errorMsg.setText("Wybrano takie same kolory!");
+        }
+    }
+
+    private String toRGBCode(Color color) {
+        return String.format( "#%02X%02X%02X",
+                (int)( color.getRed() * 255 ),
+                (int)( color.getGreen() * 255 ),
+                (int)( color.getBlue() * 255 ) );
+    }
+
+    private boolean validateColors(String color1, String color2) {
+        if(color1.equals(color2)) {
+            return true;
+        }
+        return false;
     }
 }
